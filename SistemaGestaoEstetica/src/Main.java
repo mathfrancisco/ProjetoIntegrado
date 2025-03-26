e05da1736c753045626ad6282d6247585618a163
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-03-26 17:48:06 -0300

    Create Main.java

---
+++ SistemaGestaoEstetica/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gestão para Microempresa de Estética iniciado...");
+        // Aqui pode-se adicionar chamadas de serviços e interações com o usuário
+    }
+}
